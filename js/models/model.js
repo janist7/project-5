cb806e0c78a9bf4ded6ad2c83b2135db85b69456
Janis Tidrikis <janis.tidrikis@delfi.lv>
2017-11-01 16:38:20 +0200

    Remove reliance on forsquare data

--- js/models/model.js
+++ js/models/model.js
@@ -1,25 +1,61 @@
 var model = {
     locations: [
         {
-            id: "4bd6c4dbcfa7b713d6fe27da"
+            name: "Riga Motor Museum (Rīgas Motormuzejs)",
+            id: "4bd6c4dbcfa7b713d6fe27da",
+            latLng: {
+                lat: 56.9704931657045,
+                lng: 24.227891882069482
+            }
+
         },
         {
-            id: "4c0149af6f422d7f4000411e"
+            name: "Mežaparks",
+            id: "4c0149af6f422d7f4000411e",
+            latLng: {
+                lat: 57.00924369694716,
+                lng: 24.146747589111328
+            }
         },
         {
-            id: "4bbf8bd5b492d13acad6a260"
+            name: "Vērmanes Garden (Vērmanes dārzs)",
+            id: "4bbf8bd5b492d13acad6a260",
+            latLng: {
+                lat: 56.95150834119123,
+                lng: 24.11909552246066
+            }
         },
         {
-            id: "4bbf8f54abf4952126dabd93"
+            name: "Bastion hill (Bastejkalns)",
+            id: "4bbf8f54abf4952126dabd93",
+            latLng: {
+                lat: 56.952666673937806,
+                lng: 24.109803738705583
+            }
         },
         {
-            id: "4bab71c9f964a520e9a93ae3"
+            name: "Kronvalda parks",
+            id: "4bab71c9f964a520e9a93ae3",
+            latLng: {
+                lat: 56.956246994214524,
+                lng: 24.104436903872806
+            }
         },
         {
-            id: "4bbf89184cdfc9b6346e9121"
+            name: "Dome square (Doma laukums)",
+            id: "4bbf89184cdfc9b6346e9121",
+            latLng: {
+                lat: 56.949444826828376,
+                lng: 24.10576343536377
+            }
         },
         {
-            id: "4b5f1ef2f964a52040a729e3"
+            name: "Vincents",
+            id: "4b5f1ef2f964a52040a729e3",
+            latLng: {
+                lat: 56.958775433479104,
+                lng: 24.10452533316392
+            }
         }
     ]
 };
--- js/view-models/index/indexViewModel.js
+++ js/view-models/index/indexViewModel.js
@@ -2,6 +2,9 @@ var indexViewModel = {
 
     init: function () {
         "use strict";
+        /*global google */
+        /*global styles*/
+        /*global ko*/
         var map;
         map = new google.maps.Map(document.getElementById('map'), {
             center: {
@@ -19,13 +22,15 @@ var indexViewModel = {
             }
         });
 
-        ko.applyBindings(new indexViewModel.render(map))
+        ko.applyBindings(new indexViewModel.render(map));
     }, // init
 
 
     render: function (map) {
         "use strict";
         var self = this;
+        /*global $*/
+        /*global model*/
 
         /* Variables */
         var polygon = null;
@@ -35,63 +40,46 @@ var indexViewModel = {
                 country: "lv"
             }
         };
-        var largeInfowindow = new google.maps.InfoWindow();
+        var largeInfowindow = new google.maps.InfoWindow({maxWidth: 200});
         var drawingManager;
+        var clientId = "ADD231Y2455M2GT0IBKL53WH2B52VDF3EJDT0FCLLOGAC5I4";
+        var client_secret = "FGT1TFGQSEAO3DU3ISGF50GDWASJAM5BF50AE1G5AVBT5R4U";
+        var version = "20171030";
         // Variables
 
         /* Observables */
         self.markers = ko.observableArray();
         self.address = ko.observable('');
         self.visibleMarkers = ko.observableArray();
-        self.ph = ko.observable('Enter your favorite area in Riga!')
+        self.ph = ko.observable('Enter your favorite area in Riga!');
         self.userInput = ko.observable('');
         // Observables
 
         self.Marker = function (dataObj) {
-            this.title = '-';
-            this.latLng = '-';
+            this.title = dataObj.name;
+            this.latLng = dataObj.latLng;
             this.pin = null;
             this.id = dataObj.id;
+            this.forsquareDataAvailable = false;
             this.formattedAddress = [];
             this.forsquareLink = ('-');
             this.forsquareImageLink = '-';
             this.forsquareRating = '-';
             this.hours = '-';
-        }
+        };
 
-        self.forsquareAPICall = function (marker) {
-            var clientId = "ADD231Y2455M2GT0IBKL53WH2B52VDF3EJDT0FCLLOGAC5I4";
-            var client_secret = "FGT1TFGQSEAO3DU3ISGF50GDWASJAM5BF50AE1G5AVBT5R4U";
-            var version = "20171030";
-            var url = "https://api.foursquare.com/v2/venues/" + marker.id + "?client_id=" + clientId + "&client_secret=" + client_secret + "&v=" + version;
-            var result;
-            $.ajax({
-                url: url,
-                dataType: "json",
-                async: false,
-                success: function (data) {
-                    result = data.response.venue;
-                },
-                error: function (e) {
-                    result = false;
-                }
-            });
-
-            return result;
-        }
-
-        model["locations"].forEach(function (marker) {
+        model.locations.forEach(function (marker) {
             self.markers.push(new self.Marker(marker));
         });
 
         function makeMarkerIcon(markerColor) {
             var markerImage = new google.maps.MarkerImage(
-                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor +
-                '|40|_|%E2%80%A2',
+                'http://chart.googleapis.com/chart?chst=d_map_spin&chld=1.15|0|' + markerColor + '|40|_|%E2%80%A2',
                 new google.maps.Size(21, 34),
                 new google.maps.Point(0, 0),
                 new google.maps.Point(10, 34),
-                new google.maps.Size(21, 34));
+                new google.maps.Size(21, 34)
+            );
             return markerImage;
         }
         var defaultIcon = makeMarkerIcon('0091ff');
@@ -105,14 +93,15 @@ var indexViewModel = {
                 bounds.extend(marker.pin.position);
             });
             map.fitBounds(bounds);
-        }
+        };
 
         /* This function will loop through the listings and hide them all. */
         self.hideMarkers = function () {
+            self.visibleMarkers.removeAll();
             self.markers().forEach(function (marker) {
                 marker.pin.setMap(null);
             });
-        }
+        };
 
         /* Initialize the drawing manager. */
         drawingManager = new google.maps.drawing.DrawingManager({
@@ -120,57 +109,50 @@ var indexViewModel = {
             drawingControl: true,
             drawingControlOptions: {
                 position: google.maps.ControlPosition.TOP_LEFT,
-                drawingModes: [
-              google.maps.drawing.OverlayType.POLYGON
-            ]
+                drawingModes: [google.maps.drawing.OverlayType.POLYGON]
             }
         });
 
         /* The following group uses the location array to create an array of markers on initialize. */
         self.markers().forEach(function (marker) {
-            var result = self.forsquareAPICall(marker);
-            if (result.name) {
-                marker.title = result.name;
-            }
-            var location = new google.maps.LatLng(
-                result.location.lat,
-                result.location.lng
-            );
-            marker.latLng = location;
-            if (result.location) {
-                marker.formattedAddress = result.location.hasOwnProperty('formattedAddress') ? result.location.formattedAddress : "-";
-            }
-            if (result.canonicalUrl) {
-                marker.forsquareLink = result.canonicalUrl;
-            }
-            if (result.bestPhoto) {
-                marker.forsquareImageLink = result.bestPhoto.prefix + "100x100" + result.bestPhoto.sufix;
-            }
-            if (result.rating) {
-                marker.forsquareRating = result.rating;
-            }
-            if (result.hours) {
-                marker.hours = result.hours.hasOwnProperty('statuss') ? result.hours.status : "-";
-            }
+            var url = "https://api.foursquare.com/v2/venues/" + marker.id + "?client_id=" + clientId + "&client_secret=" + client_secret + "&v=" + version;
             var markerOptions = {
                 title: marker.title,
                 map: self.googleMap,
                 position: marker.latLng,
                 animation: google.maps.Animation.DROP,
-                icon: defaultIcon,
+                icon: defaultIcon
             };
+            $.ajax({
+                url: url,
+                dataType: "json",
+                async: true,
+                success: function (data) {
+                    var result = data.response.venue;
+                    if (result.location) {
+                        marker.formattedAddress = result.location.hasOwnProperty('formattedAddress') ? result.location.formattedAddress : "-";
+                    }
+                    if (result.canonicalUrl) {
+                        marker.forsquareLink = result.canonicalUrl;
+                    }
+                    if (result.bestPhoto) {
+                        marker.forsquareImageLink = result.bestPhoto.prefix + "100x100" + result.bestPhoto.suffix;
+                    }
+                    if (result.rating) {
+                        marker.forsquareRating = result.rating;
+                    }
+                    if (result.hours) {
+                        marker.hours = result.hours.hasOwnProperty('status') ? result.hours.status : "-";
+                    }
+                    marker.forsquareDataAvailable = true;
+                },
+                error: function (e) {
+                    marker.forsquareDataAvailable = false;
+                }
+            });
             marker.pin = new google.maps.Marker(markerOptions);
-            var contentString = '<h3>' + marker.title + '</h3>' +
-            '<hr>' + '<div class="forsquare">' +
-            '<p> Address: ' +
-            marker.formattedAddress[0] + ", " +
-            marker.formattedAddress[1] + ", " +
-            marker.formattedAddress[2] + '</p>' +
-            '<p> Rating: ' + marker.forsquareRating + '</p>' +
-            '</div>' +
-            '<hr>' + '<div id="pano"></div>';
             marker.pin.addListener('click', function () {
-                self.populateInfoWindow(this, largeInfowindow, contentString);
+                self.populateInfoWindow(marker, largeInfowindow);
             });
             marker.pin.addListener('mouseover', function () {
                 this.setIcon(highlightedIcon);
@@ -179,21 +161,21 @@ var indexViewModel = {
                 this.setIcon(defaultIcon);
             });
         });
-
         self.showMarkers();
-
         self.markers().forEach(function (marker) {
             self.visibleMarkers.push(marker);
         });
 
         /* Autocomplete for zoomToArea. Limited only to Latvia. */
         var zoomAutocomplete = new google.maps.places.Autocomplete(
-            document.getElementById('zoom-to-area-text'), options);
+            document.getElementById('zoom-to-area-text'),
+            options
+        );
 
         /* Zooms to location based on address given, needs to unwrap the observable first before the value can be used */
         self.zoomToArea = function () {
             var geocoder = new google.maps.Geocoder();
-            if (ko.utils.unwrapObservable(self.address) == '') {
+            if (ko.utils.unwrapObservable(self.address) === '') {
                 window.alert('You must enter an area, or address.');
             } else {
                 geocoder.geocode({
@@ -202,7 +184,7 @@ var indexViewModel = {
                         locality: 'Latvia'
                     }
                 }, function (results, status) {
-                    if (status == google.maps.GeocoderStatus.OK) {
+                    if (status === google.maps.GeocoderStatus.OK) {
                         map.setCenter(results[0].geometry.location);
                         map.setZoom(16);
                     } else {
@@ -211,49 +193,79 @@ var indexViewModel = {
                     }
                 });
             }
-        }
+        };
 
         /* This function populates the infowindow when the marker is clicked. We'll only allow
         one infowindow which will open at the marker that is clicked, and populate based
         on that markers position. */
-        self.populateInfoWindow = function (pin, infowindow, contentString) {
-            if (infowindow.marker != pin) {
+        self.populateInfoWindow = function (marker, infowindow) {
+            if (infowindow.marker !== marker.pin) {
                 infowindow.setContent('');
-                infowindow.marker = pin;
-                pin.setAnimation(google.maps.Animation.BOUNCE);
+                infowindow.marker = marker.pin;
+                marker.pin.setAnimation(google.maps.Animation.BOUNCE);
                 setTimeout(function () {
-                    pin.setAnimation(null);
+                    marker.pin.setAnimation(null);
                 }, 1420);
                 infowindow.addListener('closeclick', function () {
                     infowindow.marker = null;
                 });
                 var streetViewService = new google.maps.StreetViewService();
                 var radius = 50;
-
-                function getStreetView(data, status) {
-                    if (status == google.maps.StreetViewStatus.OK) {
+                var contentString;
+                if (marker.forsquareDataAvailable) {
+                    contentString = '<h4>' + marker.title + '</h4>' +
+                        '<hr>' + '<div class="forsquare">' +
+                        '<img src=' + marker.forsquareImageLink + ' alt="Forsquare image"></img>' +
+                        '<p> Address: ' +
+                        marker.formattedAddress[0] + ", " +
+                        marker.formattedAddress[1] + ", " +
+                        marker.formattedAddress[2] + '</p>' +
+                        '<p>' + marker.hours + '</p>' +
+                        '<p> User rating: ' + marker.forsquareRating + '</p>' +
+                        '<a href="' + marker.forsquareLink + '">Page on forsquare</a>' +
+                        '</div>' +
+                        '<hr>';
+                } else {
+                    contentString = '<h4>' + marker.title + '</h4>' +
+                        '<hr>' +
+                        '<h5>Forsquare data not available. Refresh page later.</h5>' +
+                        '<hr>';
+                }
+                var getStreetView = function (data, status) {
+                    var panorama;
+                    var panoramaOptions;
+                    if (status === google.maps.StreetViewStatus.OK) {
+                        contentString += '<div id="pano"></div>';
                         var nearStreetViewLocation = data.location.latLng;
                         var heading = google.maps.geometry.spherical.computeHeading(
-                            nearStreetViewLocation, pin.position);
-                        var panoramaOptions = {
+                            nearStreetViewLocation,
+                            marker.pin.position
+                        );
+                        panoramaOptions = {
                             position: nearStreetViewLocation,
                             pov: {
                                 heading: heading,
                                 pitch: 30
                             }
                         };
-                        var panorama = new google.maps.StreetViewPanorama(
-                            document.getElementById('pano'), panoramaOptions);
+                        panorama = true;
                     } else {
-                        infowindow.setContent('<div>' + pin.title + '</div>' +
-                            '<div>No Street View Found</div>');
+                        contentString += '<h5>No google streets view available</h5>';
+                        panorama = false;
                     }
-                }
-                streetViewService.getPanoramaByLocation(pin.position, radius, getStreetView);
+                    infowindow.setContent(contentString);
+                    if (panorama) {
+                        panorama = new google.maps.StreetViewPanorama(
+                            document.getElementById('pano'),
+                            panoramaOptions
+                        );
+                    }
+                };
                 infowindow.setContent(contentString);
-                infowindow.open(map, pin);
+                streetViewService.getPanoramaByLocation(marker.pin.position, radius, getStreetView);
+                infowindow.open(map, marker.pin);
             }
-        }
+        };
 
         // This shows and hides (respectively) the drawing options.
         self.toggleDrawing = function () {
@@ -262,11 +274,12 @@ var indexViewModel = {
                 if (polygon !== null) {
                     polygon.setMap(null);
                     self.showMarkers();
+                    self.filterMarkers();
                 }
             } else {
                 drawingManager.setMap(map);
             }
-        }
+        };
 
         // Add an event listener so that the polygon is captured,  call the
         // searchWithinPolygon function. This will show the markers in the polygon,
@@ -288,16 +301,17 @@ var indexViewModel = {
         // and shows only the ones within it. This is so that the
         // user can specify an exact area of search.
         self.searchWithinPolygon = function () {
+            self.visibleMarkers.removeAll();
             self.markers().forEach(function (marker) {
                 // Just giving marker.latLng does not work for some odd reason, no errors too
-                var location = new google.maps.LatLng(marker.latLng.lat, marker.latLng.lng);
-                if (google.maps.geometry.poly.containsLocation(location, polygon)) {
+                if (google.maps.geometry.poly.containsLocation(marker.pin.position, polygon)) {
                     marker.pin.setMap(map);
+                    self.visibleMarkers.push(marker);
                 } else {
                     marker.pin.setMap(null);
                 }
             });
-        }
+        };
 
         self.filterMarkers = function () {
             var searchInput = self.userInput().toLowerCase();
@@ -322,7 +336,11 @@ var indexViewModel = {
 
         self.showInfoWindow = function (marker) {
             google.maps.event.trigger(marker.pin, 'click');
-            self.hideElements();
+        };
+
+        self.showMarkersAndFilter = function () {
+            self.showMarkers();
+            self.filterMarkers();
         };
     } // render
-}
+};
