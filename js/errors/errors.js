60c39c1cb2e9770608548220fd06be5b0eee5052
Janis Tidrikis <janis.tidrikis@delfi.lv>
2017-11-03 10:02:46 +0200

    Fix google maps error handler not working

--- js/errors/errors.js
+++ js/errors/errors.js
@@ -1,4 +1,4 @@
     function googleMapsError() {
         "use strict";
-        document.getElementById('error').innerHTML = "<h1>Google maps could not load.</h1>";
+        document.getElementById('error').innerHTML = "<h1>Google maps could not load </h1><p>Refresh the page some time later.</p>";
     }
